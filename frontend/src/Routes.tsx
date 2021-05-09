376c83714244f54a0c45ed0a3bbde890c95b2030
douglasmoura <douglasmoura2@outlook.com>
2021-05-09 13:56:52 -0300

    correction

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,11 +1,8 @@
 import Dashboard from "pages/Dashboard";
 import Home from "pages/Home";
-import React from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 function Routes() {
     return (
-        <div>
-
             <BrowserRouter>
                <Switch>
                    <Route path="/" exact>
@@ -16,7 +13,6 @@ function Routes() {
                    </Route>
                </Switch>
             </BrowserRouter> 
-      </div>
     );
   }
   
